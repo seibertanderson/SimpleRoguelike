d23664986d8975e705c5696d0abcaa295d2d7576
Anderson Seibert <andersonseibert0@gmail.com>
2018-11-28 22:22:21 -0200

    Initial

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    private Vector2 velocity;
+
+    public Transform player;
+
+    public float smoothTimeX = .2f;
+    public float smoothTimeY = .2f;
+
+    private void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.position.y, ref velocity.y, smoothTimeY);
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public int playerLife;
+    public Slider lifeBar;
+
+    private Rigidbody2D rb2d;
+    public float moveQuantity = .5f;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AtualizarUI();
+        MoverJogadorTransform();
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+
+
+    public void MoverJogadorTransform()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector2(transform.position.x - moveQuantity, transform.position.y);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position = new Vector2(transform.position.x + moveQuantity, transform.position.y);
+        }
+        else if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + moveQuantity);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - moveQuantity);
+        }
+    }
+    public void AtualizarUI()
+    {
+        lifeBar.value = playerLife;
+    }
+
+    public Item item;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        GetComponent<Inventory>().AddItem(item);
+        Destroy(col.gameObject);
+    }
+}
